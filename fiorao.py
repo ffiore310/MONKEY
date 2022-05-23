039da5679cc4aa6475f980d10ec0f226be70b656
Fernando Fiore <fernandoof@al.insper.edu.br>
2022-05-23 16:55:40 -0300

    pac

--- fiorao.py
+++ fiorao.py
@@ -1,3 +1,32 @@
-import pygame
 
-print('fiorao eh o carapip')
+import pygame
+import random 
+
+largura = 900
+altura = 800
+background = pygame.image.load('assets/img/starfield.png').convert()
+
+window = pygame.display.set_mode(largura, altura)
+
+class PacMan(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, )
+        self.rect.y = random.randint(-100, )
+        self.speedx = random.randint(-3, 3)
+        self.speedy = random.randint(2, 9)
+
+    def update(self):
+        # Atualizando a posição do meteoro
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        # Se o meteoro passar do final da tela, volta para cima e sorteia
+        # novas posições e velocidades
+        if self.rect.top > altura or self.rect.right < 0 or self.rect.left > largura:
+            self.rect.x = random.randint(0, WIDTH-METEOR_WIDTH)
+            self.rect.y = random.randint(-100, -METEOR_HEIGHT)
+            self.speedx = random.randint(-3, 3)
+            self.speedy = random.randint(2, 9)
+a = 0
