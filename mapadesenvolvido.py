1ad05af8e23374a11127be7a78b0c70e23a898c2
Arthur Calabresi <arthurec@al.insper.edu.br>
2022-05-30 17:48:56 -0300

    PACMAN dentro do mapa

--- mapadesenvolvido.py
+++ mapadesenvolvido.py
@@ -1,12 +1,13 @@
 from mapa import MAPA
 import pygame
-from classes import Bloco
+from classes import Bloco, Pacman02
 
 pygame.init()
 
+# DEFINICAO MAPA
+
 BLOCO_ALTURA = 20
 BLOCO_LARGURA = 20
-
 ALTURA = len(MAPA[0]) * 20
 LARGURA = len(MAPA) * BLOCO_LARGURA
 
@@ -17,24 +18,77 @@ img_bloco = pygame.image.load('assets/img/bloco_jogo_pacman.jpg').convert()
 img_bloco = pygame.transform.scale(img_bloco, (20,20))
 mapa_com_blocos = pygame.sprite.Group()
 
-
 for l in range(len(MAPA)):
     for c in range(len(MAPA[l])):
         if MAPA[l][c] == 0:
             bloco = Bloco(img_bloco, c * 20, l * 20)
             mapa_com_blocos.add(bloco)
+
+# IMAGENS PACMAN
+
+pac_img_fechado = pygame.image.load('assets/img/pacman_fechado.png')
+pac_img_fechado = pygame.transform.scale(pac_img_fechado, (50,50))
+pac_img_aberto = pygame.image.load('assets/img/pacman_direita.png')
+pac_img_aberto = pygame.transform.scale(pac_img_aberto, (50,50))
+pac_img_aberto_esquerda = pygame.image.load('assets/img/pacman_esquerdo .png')
+pac_img_aberto_esquerda = pygame.transform.scale(pac_img_aberto_esquerda, (50,50))
+
+pac_img_aberto_cima = pygame.image.load('assets/img/pacman_cima.png')
+pac_img_aberto_cima = pygame.transform.scale(pac_img_aberto_cima, (50,50))
+
+pac_img_aberto_baixo = pygame.image.load('assets/img/pacman_baixo.png')
+pac_img_aberto_baixo = pygame.transform.scale(pac_img_aberto_baixo, (50,50))
+
+paclist_img = [pac_img_fechado, pac_img_aberto,pac_img_aberto_esquerda, pac_img_aberto_cima, pac_img_aberto_baixo]
+
 game= True
 
+#ACERTANDO OS FPS
+clock = pygame.time.Clock()
+FPS = 10
 
+# CRIANDO PERSONAGENS
+player = Pacman02(paclist_img)
+
+all_sprites = pygame.sprite.Group
+all_sprites.add(player)
+    
+
+# INICIANDO O JOGO
 while game:
-    # ----- Trata eventos
+    clock.tick(FPS)
+
+    # EVENTOS
     for event in pygame.event.get():
-        # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                player.speedx += 8
+            if event.key == pygame.K_a:
+                player.speedx -= 8
+            if event.key == pygame.K_w:
+                player.speedy -= 8
+            if event.key == pygame.K_s:
+                player.speedy += 8
 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:
+                player.speedx -= 8
+            if event.key == pygame.K_a:
+                player.speedx += 8
+            if event.key == pygame.K_w:
+                player.speedy += 8
+            if event.key == pygame.K_s:
+                player.speedy -= 8
+
+    #ATUALIZA O JOGO
     mapa_com_blocos.update()
+    player.update(paclist_img)
+
+    # GERA SAIDAS
     window.fill((0,0,0))
+    window.blit(player.image, player.rect)
 
     mapa_com_blocos.draw(window)
     pygame.display.update()
