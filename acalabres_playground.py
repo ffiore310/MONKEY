dd0cf3f05d17babd2664d5b6c101fdeabaabc808
Arthur Calabresi <arthurec@al.insper.edu.br>
2022-06-01 17:41:44 -0300

    colisoes pacman parede+ teletransporte

--- acalabres_playground.py
+++ acalabres_playground.py
@@ -1,86 +1,71 @@
 import pygame
-mapa = [
-    '0000000000000000000000000000000000',
-    '0               00               0',
-    '0   0   0 0 0    0'
-]
+import random
+from mapa import MAPA
+from config import *
 
-for l in range(len(mapa)):
-    for c in range(len(mapa[l])):
-        if mapa[l][c] == '0':
-            b = Bloco(img, l * 20, c * 20)
-            all_blocks.add(b)
+class Pacman02 (pygame.sprite.Sprite):
 
-class Pacman (pygame.sprite.Sprite):
-
-    def __init__(self, images):
-
-        self.index_image = 0
-        self.images = images
-        self.image = self.images[self.index_image]
+    def __init__(self, lista_img):
+        pygame.sprite.Sprite.__init__(self)
+        self.e = 0
+        self.lista_img = lista_img
+        self.image = lista_img[self.e]
         self.rect = self.image.get_rect()
-        self.rect.x = 10
-        self.rect.y = 10
+        self.rect.x = 30
+        self.rect.y = 330
         self.speedx = 0
         self.speedy = 0
+        self.last = 1
+        self.last_update = pygame.time.get_ticks()
+        self.frame_ticks = 50
 
-    def update(self): 
-        self.index_image = (self.index_image + 1) % len(self.images)
-        self.image = self.images[self.index_image]
-        self.rect = self.image.get_rect()
-        self.rect.x = 10
-        self.rect.y = 10
-
+    def update(self):
         self.rect.x += self.speedx 
         self.rect.y += self.speedy
 
+        now = pygame.time.get_ticks()
+        elapsed_ticks = now - self.last_update
 
-game = True
+        if elapsed_ticks > self.frame_ticks:
+            self.last_update = now
+        
+        if self.speedx > 0:
+            self.last = 1
+            self.e = (self.e -1 )* (-1)
+            if self.e == 0:
+                self.image = self.lista_img[0]
+            else:
+                self.image = self.lista_img[1]
 
-clock = pygame.time.Clock()
-FPS = 30
+        if self.speedx < 0:
+            self.last = 2
+            self.e = (self.e -1 )* (-1)
+            if self.e == 0:
+                self.image = self.lista_img[0]
+            else:
+                self.image = self.lista_img[2]
 
+        if self.speedy > 0:
+            self.last = 4
+            self.e = (self.e -1 )* (-1)
+            if self.e == 0:
+                self.image = self.lista_img[0]
+            else:
+                self.image = self.lista_img[4]
 
-player = Pacman(pac_img)
+        if self.speedy < 0:
+            self.last = 3
+            self.e = (self.e -1 )* (-1)
+            if self.e == 0:
+                self.image = self.lista_img[0]
+            else:
+                self.image = self.lista_img[3]
 
-all_sprites = pygame.sprite.Group()
-
-all_sprites.add(player)
-
-while game:
-    clock.tick(FPS)
-
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            game = False
-
-        if event.type == pygame.KEYDOWN:
-
-            if event.key == pygame.K_RIGHT:
-                player.speedx += 8
-
-            if event.key == pygame.K_LEFT:
-                player.speedx -= 8
-
-            if event.key == pygame.K_UP:
-                player.speedy += 8
-            
-            if event.type == pygame.K_DOWN:
-                player.speedy -= 8
-
-        if event.type == pygame.KEYUP:
-
-            if event.key == pygame.K_RIGHT:
-                player.speedx -= 8
-
-            if event.key == pygame.K_LEFT:
-                player.speedx += 8
-
-            if event.key == pygame.K_UP:
-                player.speedy -= 8
-            
-            if event.type == pygame.K_DOWN:
-                player.speedy += 8
-
-    all_sprites.update()
+        if self.speedy == 0 and self.speedx == 0:
+            self.e = (self.e -1 )* (-1)
+            if self.e == 0:
+                self.image = self.lista_img[0]
+            else:
+                self.image = self.lista_img[self.last]
+        
+        # COLISAO COM O MAPA
--- fake_main.py
+++ fake_main.py
@@ -1,45 +1,74 @@
+import random
+from mapa import MAPA
 import pygame
-from classe_pacman import Pacman, Pacman02
-
-# INICIANDO O JOGO
+from classes import Bloco, Fantasma
+from config import *
+from acalabres_playground import Pacman02
 
 pygame.init()
 
-#CRIANDO A JANELA
-largura = 500
-altura = 500
-window = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Fake main- PACMAN')
+# DEFINICAO MAPA
 
-#DEFININDO IMAGENS
+window = pygame.display.set_mode((ALTURA, LARGURA))
+pygame.display.set_caption('PAC-MAN')
+
+img_bloco = pygame.image.load('assets/img/bloco_jogo_pacman.jpg').convert()
+img_bloco = pygame.transform.scale(img_bloco, (30,30))
+mapa_com_blocos = pygame.sprite.Group()
+
+all_sprites = pygame.sprite.Group()
+all_fantasmas = pygame.sprite.Group()
+
+
+for l in range(len(MAPA)):
+    for c in range(len(MAPA[l])):
+        if MAPA[l][c] == 0:
+            bloco = Bloco(img_bloco, c, l)
+            mapa_com_blocos.add(bloco)
+            all_sprites.add(bloco)
+
+# IMAGENS PACMAN
+
 pac_img_fechado = pygame.image.load('assets/img/pacman_fechado.png')
-pac_img_fechado = pygame.transform.scale(pac_img_fechado, (50,50))
+pac_img_fechado = pygame.transform.scale(pac_img_fechado, (30,30))
 pac_img_aberto = pygame.image.load('assets/img/pacman_direita.png')
-pac_img_aberto = pygame.transform.scale(pac_img_aberto, (50,50))
+pac_img_aberto = pygame.transform.scale(pac_img_aberto, (30,30))
 pac_img_aberto_esquerda = pygame.image.load('assets/img/pacman_esquerdo .png')
-pac_img_aberto_esquerda = pygame.transform.scale(pac_img_aberto_esquerda, (50,50))
+pac_img_aberto_esquerda = pygame.transform.scale(pac_img_aberto_esquerda, (30,30))
 
 pac_img_aberto_cima = pygame.image.load('assets/img/pacman_cima.png')
-pac_img_aberto_cima = pygame.transform.scale(pac_img_aberto_cima, (50,50))
+pac_img_aberto_cima = pygame.transform.scale(pac_img_aberto_cima, (30,30))
 
 pac_img_aberto_baixo = pygame.image.load('assets/img/pacman_baixo.png')
-pac_img_aberto_baixo = pygame.transform.scale(pac_img_aberto_baixo, (50,50))
+pac_img_aberto_baixo = pygame.transform.scale(pac_img_aberto_baixo, (30,30))
 
 paclist_img = [pac_img_fechado, pac_img_aberto,pac_img_aberto_esquerda, pac_img_aberto_cima, pac_img_aberto_baixo]
 
-#DEFININDO PARAMETROS
-player = Pacman02(paclist_img)
+img_fantasmas = []
+for name in ['fantasma_azul.png', 'fantasma_laranja.png', 'fantasma_rosa.png', 'fantasma_vermelho.png']:
+    img = pygame.image.load(f'assets/img/{name}')
+    img = pygame.transform.scale(img, (30,30))
+    img_fantasmas.append(img)
 
-all_sprites = pygame.sprite.Group
-all_sprites.add(player)
-    
-game = True
+game= True
 
 #ACERTANDO OS FPS
 clock = pygame.time.Clock()
 FPS = 10
 
-#INICIANDO A LOGICA DO JOGO
+# CRIANDO PERSONAGENS
+player = Pacman02(paclist_img)
+last_list = [0,0]
+
+all_sprites.add(player)
+lugar_inicial_fantasma = 13   
+for fantasmas in img_fantasmas:
+    f = Fantasma(fantasmas,lugar_inicial_fantasma, 11)
+    all_sprites.add(f)
+    all_fantasmas.add(f)
+    lugar_inicial_fantasma += 3
+
+# INICIANDO O JOGO
 while game:
     clock.tick(FPS)
 
@@ -59,26 +88,39 @@ while game:
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_d:
-                player.speedx -= 8
+                player.speedx = 0
             if event.key == pygame.K_a:
-                player.speedx += 8
+                player.speedx = 0
             if event.key == pygame.K_w:
-                player.speedy += 8
+                player.speedy = 0
             if event.key == pygame.K_s:
-                player.speedy -= 8
+                player.speedy = 0
 
-    # ATUALIZAR O JOGO
-    player.update(paclist_img)
+    #ATUALIZA O JOGO
+    all_sprites.update()
 
-    #GERA SAIDAS
-    window.fill((7, 3, 252))
+    last_list.append(player.rect.x)
+    last_list.append(player.rect.y)
+
+    
+    hits = pygame.sprite.spritecollide(player,mapa_com_blocos, False)
+    if len(hits)>0:
+        player.speedx =0 
+        player.speedy =0 
+        player.rect.x = last_list[2] 
+        player.rect.y = last_list[3] 
+        print('SPEEDY {}'.format(player.speedy))
+        print('SPEEDX {}'.format(player.speedx))
+
+    print(last_list)
+
+    del last_list[0]
+    del last_list[1]
+    print(last_list)
+    # GERA SAIDAS
+    window.fill((0,0,0))
     window.blit(player.image, player.rect)
 
+    all_sprites.draw(window)
     pygame.display.update()
 
-
-
-
-
-
-pygame.quit()
